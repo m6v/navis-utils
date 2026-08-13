61f2754997841a0809cfb780ae3da1e8541f506c
Sergey Maksimov <m6v@mail.ru>
2026-08-13 08:49:57 +0300

    Minor changes

--- timemachine/timemachine.sh
+++ timemachine/timemachine.sh
@@ -11,9 +11,9 @@ case "$1" in
             exit 1
         fi
 
-        # Считываем сохраненное виртуальное время из конфига
+        # Чтение виртуального времени из конфига
         TARGET_TIME=$(cat "$CONF" | tr -d '[:space:]')
-        echo "Запуск TimeMachine контейнера. Целевое виртуальное время: $TARGET_TIME"
+        echo "Запуск TimeMachine контейнера. Виртуальное время: $TARGET_TIME"
         
         # Запускаем контейнер и передаем ему timestamp аргументом
         exec systemd-nspawn -D "$MERGED_DIR" -M "$CONTAINER_NAME" \
@@ -22,22 +22,23 @@ case "$1" in
         ;;
 
     stop)
-        echo "TimeMachine: фиксируем виртуальное время..."
+        echo "TimeMachine: фиксация виртуального времени..."
 
         if [ -f "$CONF" ]; then
             START_TIME=$(cat "$CONF" | tr -d '[:space:]')
             
-            # Находим PID процесса sleep infinity
+            # Поиск PID процесса sleep infinity
             SLEEP_PID=$(pgrep -f "sleep infinity")
             
             if [ -n "$SLEEP_PID" ]; then
-                # 1. Получаем общий аптайм хоста с момента включения
+                # Получение общего аптайма хоста с момента включения
                 HOST_UPTIME=$(awk '{print int($1)}' /proc/uptime)
                 
-                # 2. Получаем секунду старта процесса с момента включения хоста
+                # Получение секунды старта процесса с момента включения хоста
                 PROCESS_START=$(awk '{print int($22 / cv)}' cv=$(getconf CLK_TCK) /proc/$SLEEP_PID/stat 2>/dev/null)
                 
-                # 3. Настоящий аптайм процесса — это их РАЗНИЦА!
+                # Вычисление истинного аптайма процесса как разности
+                # между аптайма хоста и старта процесса с момента включения хоста
                 if [ -n "$HOST_UPTIME" ] && [ -n "$PROCESS_START" ]; then
                     UPTIME_SEC=$((HOST_UPTIME - PROCESS_START))
                     VIRTUAL_TIME=$((START_TIME + UPTIME_SEC))
@@ -47,12 +48,12 @@ case "$1" in
 
         if [ -n "$VIRTUAL_TIME" ] && [[ "$VIRTUAL_TIME" =~ ^[0-9]+$ ]]; then
             echo "$VIRTUAL_TIME" > "$CONF"
-            echo "Время успешно заморожено в $CONF: $VIRTUAL_TIME"
+            echo "Время успешно сохранено в $CONF: $VIRTUAL_TIME"
         else
             echo "Предупреждение: Не удалось рассчитать аптайм, конфиг не изменен."
         fi
 
-        echo "Останавливаем контейнер..."
+        echo "Остановка контейнера..."
         machinectl terminate "$CONTAINER_NAME" 2>/dev/null
         ;;
 esac
