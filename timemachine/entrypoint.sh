9262cc07a49965d197988ee662b1896164a26cd1
Sergey Maksimov <m6v@mail.ru>
2026-08-13 13:23:39 +0300

    Add sharing /srv/repo by http

--- timemachine/entrypoint.sh
+++ timemachine/entrypoint.sh
@@ -8,6 +8,11 @@ sleep 0.5
 /sbin/ip addr add 10.0.0.254/24 dev host0 2>/dev/null
 /sbin/ip link set dev host0 up 2>/dev/null
 
+# Запуск веб-сервера для раздачи репозитория Astra Linux в виртуальные машины УТК-СЗИ
+# в sources.list добавить deb http://10.0.0.254/alse/main 1.7_x86-84 contrib main non-free
+echo "TimeMachine: Запуск веб-сервера репозитория на порту 80..."
+python3 -m http.server --directory /srv/repo 80 &
+
 TARGET_DATE=$(date -d "@$TARGET_TIME" +"%Y-%m-%d %H:%M:%S")
 
 # Запуск Chrony на переднем плане (-d) в фоне самого bash (&)
--- timemachine/timemachine.sh
+++ timemachine/timemachine.sh
@@ -11,37 +11,27 @@ case "$1" in
             exit 1
         fi
 
-        # Чтение виртуального времени из конфига
         TARGET_TIME=$(cat "$CONF" | tr -d '[:space:]')
         echo "Запуск TimeMachine контейнера. Виртуальное время: $TARGET_TIME"
-        
-        # Запускаем контейнер и передаем ему timestamp аргументом
-        exec systemd-nspawn --keep-unit  -M "$CONTAINER_NAME" \
-             -D "$MERGED_DIR" /entrypoint.sh "$TARGET_TIME"
+        exec systemd-nspawn --keep-unit -M "$CONTAINER_NAME" -D "$MERGED_DIR" /entrypoint.sh "$TARGET_TIME"
         ;;
 
     stop)
         echo "TimeMachine: фиксация виртуального времени..."
 
-        if [ -f "$CONF" ]; then
+        if [ -f "$CONF" ] && [ -n "$MAINPID" ] && [ "$MAINPID" -gt 0 ]; then
             START_TIME=$(cat "$CONF" | tr -d '[:space:]')
             
-            # Поиск PID процесса sleep infinity
-            SLEEP_PID=$(pgrep -f "sleep infinity")
+            # Получаем общий аптайма хоста
+            HOST_UPTIME=$(awk '{print int($1)}' /proc/uptime)
             
-            if [ -n "$SLEEP_PID" ]; then
-                # Получение общего аптайма хоста с момента включения
-                HOST_UPTIME=$(awk '{print int($1)}' /proc/uptime)
-                
-                # Получение секунды старта процесса с момента включения хоста
-                PROCESS_START=$(awk '{print int($22 / cv)}' cv=$(getconf CLK_TCK) /proc/$SLEEP_PID/stat 2>/dev/null)
-                
-                # Вычисление истинного аптайма процесса как разности
-                # между аптайма хоста и старта процесса с момента включения хоста
-                if [ -n "$HOST_UPTIME" ] && [ -n "$PROCESS_START" ]; then
-                    UPTIME_SEC=$((HOST_UPTIME - PROCESS_START))
-                    VIRTUAL_TIME=$((START_TIME + UPTIME_SEC))
-                fi
+            # Получаем секунду старта САМОГО КОНТЕЙНЕРА (его Main PID) из /proc
+            PROCESS_START=$(awk '{print int($22 / cv)}' cv=$(getconf CLK_TCK) /proc/$MAINPID/stat 2>/dev/null)
+            
+            if [ -n "$HOST_UPTIME" ] && [ -n "$PROCESS_START" ]; then
+                # Вычисляем, сколько секунд РЕАЛЬНО проработал контейнер
+                UPTIME_SEC=$((HOST_UPTIME - PROCESS_START))
+                VIRTUAL_TIME=$((START_TIME + UPTIME_SEC))
             fi
         fi
 
