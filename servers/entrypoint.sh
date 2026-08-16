ea0d08a733037181addeb26c041883857134bd49
Sergey Maksimov <m6v@mail.ru>
2026-08-16 21:36:07 +0300

    Refactoring code, remove timeshifter

--- servers/entrypoint.sh
+++ servers/entrypoint.sh
@@ -5,7 +5,7 @@ TIMESHIFT_FILE="/etc/timeshift"
 TIMESTAMP_FILE="/etc/timestamp"
 
 # Настройка сети
-echo "Настройка сетевого интерфейса host0 контейнера..."
+echo "Configuring container network interface host0..."
 sleep 0.5
 /sbin/ip addr add 10.0.0.254/24 dev host0 2>/dev/null
 /sbin/ip link set dev host0 up 2>/dev/null
@@ -22,11 +22,11 @@ trap "on_exit" EXIT SIGTERM SIGINT
 
 # Запуск веб-сервера для раздачи репозитория Astra Linux в виртуальные машины УТК-СЗИ
 # в sources.list добавить deb http://10.0.0.254/alse/main 1.7_x86-84 contrib main non-free
-echo "Запуск веб-сервера репозитория на порту 80..."
+echo "Starting HTTP repository server on port 80..."
 python3 -m http.server --directory /srv/repo 80 &
 
 # Запуск chronyd на переднем плане (-d) в фоне самого bash (&)
-echo "Запуск chronyd..."
+echo "Starting chronyd in debug mode with drift correction disabled..."
 /usr/sbin/chronyd -d -x &
 
 # Вычисление времени простоя контейнера с момента последнего запуска
@@ -34,7 +34,7 @@ downtime=0
 if [ -f "$TIMESTAMP_FILE" ]; then
     downtime=$(( $(date +%s) - $(cat "$TIMESTAMP_FILE") ))
 fi
-echo "Время простоя контейнера $downtime сек"
+echo "Container downtime since last launch is ${downtime}s"
 
 # Изменение времени смещения на время простоя 
 if [ -f "$TIMESHIFT_FILE" ]; then
@@ -43,14 +43,12 @@ if [ -f "$TIMESHIFT_FILE" ]; then
 else
     timeshift=0
 fi
-echo "Смещение времени $timeshift сек"
+echo "Setting time offset to ${timeshift}s"
 
 # Вычисление и установка смещенного времени
-shited_time=$(date -d "@$(($(date +%s) + timeshift))" +"%Y-%m-%dT%H:%M:%S")
-echo "Установка смещенного времени $shited_time"
-chronyc -a settime $shited_time 
-echo "Установка запрета корректировки дрейфа часов"
-chronyc -a manual delete 0
+target_time=$(date -d "@$(($(date +%s) + timeshift))" +"%Y-%m-%dT%H:%M:%S")
+echo "Setting offset time to ${target_time}"
+chronyc -a settime $target_time 
 
 # Удержание контейнера в запущенном состоянии
 wait
