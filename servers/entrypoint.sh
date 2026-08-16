a07e4f85f974616f70a7baa61fe80e1d267a2d56
Sergey Maksimov <m6v@mail.ru>
2026-08-16 15:35:34 +0300

    Refactoring vlab-service subproject. Not tested

--- servers/entrypoint.sh
+++ servers/entrypoint.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 
+# Обязательно должен пробрасываться с хоста в контейнер
 TIMESHIFT_FILE="/etc/timeshift"
 
 # Настройка сети
-echo "TimeMachine: Настройка сетевого интерфейса host0 контейнера..."
+echo "Настройка сетевого интерфейса host0 контейнера..."
 sleep 0.5
 /sbin/ip addr add 10.0.0.254/24 dev host0 2>/dev/null
 /sbin/ip link set dev host0 up 2>/dev/null
+
+on_exit() {
+    # Здесь нельзя использовать внешние утилиты, поэтому импользуем флаг %(...)T,
+    # который умеет брать текущее Unix-время напрямую из памяти самого процесса
+    printf -v timestamp '%(%s)T' -1
+    echo "$timestamp" > /etc/timestamp
+    sync
+    exit 0
+}
+trap "on_exit" EXIT SIGTERM SIGINT
+
+downtime=$(( $(date +%s) - $(cat /etc/timestamp 2>/dev/null || echo 0) ))
+echo "Время простоя контейнера $downtime сек"
+
+# Уменьшение смещения в /etc/timeshift на время простоя 
+if [ -f "$TIMESHIFT_FILE" ]; then
+    echo $(( $(cat "$TIMESHIFT_FILE") + $downtime )) > "$TIMESHIFT_FILE"
+fi
+
+# TODO Дальше посчитать сумму смещения и текущего времени и передатьь ее в chrony
+
 
 # Запуск веб-сервера для раздачи репозитория Astra Linux в виртуальные машины УТК-СЗИ
 # в sources.list добавить deb http://10.0.0.254/alse/main 1.7_x86-84 contrib main non-free
 echo "Запуск веб-сервера репозитория на порту 80..."
 python3 -m http.server --directory /srv/repo 80 &
 
-# Запуск Chrony на переднем плане (-d) в фоне самого bash (&)
-echo "Запуск изолированного Chronyd..."
+# Запуск chronyd на переднем плане (-d) в фоне самого bash (&)
+echo "Запуск chronyd..."
 /usr/sbin/chronyd -d -x &
 
 # Проверка наличия и чтение файла со смещением времени
@@ -36,4 +58,4 @@ echo "Установка запрета корректировки дрейфа 
 chronyc -a manual delete 0
 
 # Удержание контейнера в запущенном состоянии
-exec /bin/sleep infinity
+wait
