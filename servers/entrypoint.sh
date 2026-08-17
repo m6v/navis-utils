557c654b0160451d769d6cad04c83b8c5dcc0107
Sergey Maksimov <m6v@mail.ru>
2026-08-17 23:12:42 +0300

    Simplify timeshifting fuctions

--- servers/entrypoint.sh
+++ servers/entrypoint.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-TIMESHIFT_FILE="/etc/timeshift"
-TIMESTAMP_FILE="/etc/timestamp"
+FAKETIME_FILE="/etc/faketime"
+
+start_time=$(date +%s)
+# Если смещенное время не задано, использовать текущее время
+target_time=$(cat "$FAKETIME_FILE" 2>/dev/null || echo "$start_time")
 
 # Настройка сети
 echo "Configuring container network interface host0..."
-# sleep 0.5
 /sbin/ip addr add 10.0.0.254/24 dev host0 2>/dev/null
 /sbin/ip link set dev host0 up 2>/dev/null
 
 on_exit() {
     # Здесь нельзя использовать внешние утилиты, поэтому импользуем флаг %(...)T,
     # который умеет брать текущее Unix-время напрямую из памяти самого процесса
-    printf -v timestamp '%(%s)T' -1
-    echo "$timestamp" > /etc/timestamp
-    sync
+    if [ -f "$FAKETIME_FILE" ]; then
+        printf -v stop_time '%(%s)T' -1
+        # Увеличить смещенное время на длительность работы контейнера
+        echo $(( target_time + stop_time - start_time )) > "$FAKETIME_FILE"
+        sync
+    fi
     exit 0
 }
 trap "on_exit" EXIT SIGTERM SIGINT
 
 # Запуск веб-сервера для раздачи репозитория Astra Linux в виртуальные машины УТК-СЗИ
-# в sources.list добавить deb http://10.0.0.254/alse/main 1.7_x86-84 contrib main non-free
+# В sources.list добавить "deb http://10.0.0.254/alse/main 1.7_x86-84 contrib main non-free"
 echo "Starting HTTP repository server on port 80..."
 python3 -m http.server --directory /srv/repo 80 &
 
@@ -28,26 +33,9 @@ python3 -m http.server --directory /srv/repo 80 &
 echo "Starting chronyd in debug mode with drift correction disabled..."
 /usr/sbin/chronyd -d -x &
 
-# Вычисление времени простоя контейнера с момента последнего запуска
-downtime=0
-if [ -f "$TIMESTAMP_FILE" ]; then
-    downtime=$(( $(date +%s) - $(cat "$TIMESTAMP_FILE") ))
-fi
-echo "Container downtime since last launch is ${downtime}s"
-
-# Изменение времени смещения на время простоя 
-if [ -f "$TIMESHIFT_FILE" ]; then
-    timeshift=$(cat "$TIMESHIFT_FILE")
-    (( timeshift += downtime )) && echo $timeshift > "$TIMESHIFT_FILE"
-else
-    timeshift=0
-fi
-echo "Setting time offset to ${timeshift}s"
-
-# Вычисление и установка смещенного времени
-target_time=$(date -d "@$(($(date +%s) + timeshift))" +"%Y-%m-%dT%H:%M:%S")
-echo "Setting offset time to ${target_time}"
-chronyc -a settime $target_time 
+# Установка смещенного времени
+echo "Setting offset time to $(date -d "@$target_time" +"%Y-%m-%dT%H:%M:%S")..."
+chronyc -a settime $(date -d "@$target_time" +"%Y-%m-%dT%H:%M:%S") 
 
 # Удержание контейнера в запущенном состоянии
 wait
